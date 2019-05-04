263fe18c02b98be9f634408352a6312d8fef9762
tokutatsu <s17t248@stu.kagawa-u.ac.jp>
2019-05-04 20:36:01 +0900

    チャットデザインをルーム機能に合わせて変更 #1

--- public/js/login.js
+++ public/js/login.js
@@ -4,12 +4,19 @@ const $roomId = $('#roomId');
 const $log = $('#log');
 
 $form.submit(() => {
-    if ($name.val().replace(/\s/g, '').length <= 0) {
-        $log.append($('<div/>').text('名前を入力してください'));
+    let nameLength = $name.val().replace(/\s/g, '').length;
+    let roomIdLength = $roomId.val().replace(/\s/g, '').length;
+    if (nameLength + roomIdLength <= 0) {
+        $log.text('名前を入力してください');
+        $log.append($('<div/>').text('ルームIDを入力してください'));
         return false;
     }
-    if ($roomId.val().replace(/\s/g, '').length <= 0) {
-        $log.append($('<div/>').text('ルームIDを入力してください'));
+    if (nameLength <= 0) {
+        $log.text('名前を入力してください');
+        return false;
+    }
+    if (roomIdLength <= 0) {
+        $log.text('ルームIDを入力してください');
         return false;
     }
 });
